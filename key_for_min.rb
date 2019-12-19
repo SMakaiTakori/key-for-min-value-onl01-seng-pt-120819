c2cf72279f2237f7bdcf596184dfcb8752909742
S. Makai Takori <makait87@gmail.com>
2019-12-19 16:58:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     if smallest_value == 0 || value < smallest_key
     smallest_value = value
+    smallest_key = key
     
 end
 
